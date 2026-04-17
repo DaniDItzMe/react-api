bc0349ff7d6fb8154681e03d53c45aee618d2eb7
DaniDItzMe <td.danix@gmail.com>
2026-04-18 00:41:55 +0200

    feat: exercise completed with bonuses

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,21 +5,21 @@ export default function Card({name, dateOfBirth, nationality, bio, image, awards
         <div className="card" style={{width: "18rem"}}>
             <img src={image} className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">{name}</h5>
-                <p className="card-text">{dateOfBirth} {nationality}</p>
+                <h5 className="card-title text-center">{name}</h5>
+                <p className="card-text text-center">{dateOfBirth} {nationality}</p>
             </div>
             <ul className="list-group list-group-flush">
                 <li className="list-group-item">
                     <p className="text-center fw-bold">Biography</p>
                     <p className="text-center">{bio}</p>
                 </li>
-                <li className="list-group-item">
+                <li className="list-group-item known_for">
                     <p className="text-center fw-bold">Known for</p>
                     <p className="text-center">
                         {knownFor.join(", ")}
                     </p>
                 </li>
-                <li className="list-group-item">
+                <li className="list-group-item awards">
                     <p className="text-center fw-bold">Awards</p>
                     <p className="text-center">
                         {awards.join(", ")}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,8 +2,40 @@ import Card from "./Card";
 export default function Main({ actors, actresses }) {
   return (
     <main className="container my-5">
-      <h2 className="text-center mb-3">Entire cast</h2>
+      <h2 className="mb-3">Attori</h2>
       <div className="cards-container row g-5">
+        {actors.map((actor) => (
+          <div key={actor.id} className="col-auto">
+            <Card
+              name={actor.name}
+              image={actor.image}
+              nationality={actor.nationality}
+              dateOfBirth={actor.birth_year}
+              bio={actor.biography}
+              awards={actor.awards}
+              knownFor={actor.known_for}
+            ></Card>
+          </div>
+        ))}
+      </div>
+      <div className="cards-container row g-5 mb-3">
+        <h2 className="">Attrici</h2>
+        {actresses.map((actress) => (
+          <div key={actress.id} className="col-auto">
+            <Card
+              name={actress.name}
+              image={actress.image}
+              nationality={actress.nationality}
+              dateOfBirth={actress.birth_year}
+              bio={actress.biography}
+              awards={actress.awards}
+              knownFor={actress.most_famous_movies}
+            ></Card>
+          </div>
+        ))}
+      </div>
+      <div className="cards-container row g-5">
+        <h2 className="">Tutto il cast</h2>
         {actors.map((actor) => (
           <div key={actor.id} className="col-auto">
             <Card
